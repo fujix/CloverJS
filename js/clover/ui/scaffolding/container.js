5f247c56fed158aab00a9aea2fb5fe005830b30a
OrgaChem <orga.chem.job@gmail.com>
2012-07-11 00:29:38 +0900

    Implement scaffolding module

--- js/clover/ui/scaffolding/container.js
+++ js/clover/ui/scaffolding/container.js
@@ -6,4 +6,18 @@
  * @author orga.chem.job@gmail.com (Orga Chem)
  */
 
-goog.provide('ui.scaffolding');
+goog.provide('clover.ui.scaffolding.Container');
+goog.require('goog.ui.ContainerRenderer');
+
+
+
+/**
+ *
+ * @constructor
+ * @extends {goog.ui.ContainerRenderer}
+ */
+clover.ui.scaffolding.ContainerRenderer = function() {
+  goog.base(this);
+};
+goog.inherits(clover.ui.scaffolding.ContainerRenderer, goog.ui.ContainerRenderer);
+goog.addSingletonGetter(clover.ui.scaffolding.ContainerRenderer);
