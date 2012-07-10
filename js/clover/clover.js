e348ed35b76438be3d3735251e7b999bba29b4b0
OrgaChem <orga.chem.job@gmail.com>
2012-07-10 23:30:33 +0900

    Initial commit

---
+++ js/clover/clover.js
@@ -0,0 +1,9 @@
+// This script licensed under the MIT.
+// http://orgachem.mit-license.org
+
+/**
+ * @fileoverview Script for CloverJS.
+ * @author orga.chem.job@gmail.com (Orga Chem)
+ */
+
+goog.provide('clover');
---
+++ js/clover/ui/scaffolding/container.js
@@ -0,0 +1,9 @@
+// This script licensed under the MIT.
+// http://orgachem.mit-license.org
+
+/**
+ * @fileoverview Script for UI modules for CloverJS.
+ * @author orga.chem.job@gmail.com (Orga Chem)
+ */
+
+goog.provide('ui.scaffolding');
